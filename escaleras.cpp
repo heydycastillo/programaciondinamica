43849ad7cfbe8d8f3d820f3e59fa48ab7f3a17d8
heydy <hcastilloa@ulasalle.edu.pe>
2026-06-22 22:04:25 -0500

    implementacion de la recurrencia y muestra de la tabla dp y numero de formas posibles

--- escaleras.cpp
+++ escaleras.cpp
@@ -4,14 +4,38 @@ using namespace std;
 
 int main() {
     int n;
-     //se  lee cantidad de escalones 
-    cout << "Ingrese el numero de escalones: ";
-    cin >> n;
-    // tabla dp donde almacena las formas de llegar al escalon i
+
+    cout << "ingrese el numero de escalones: ";
+    cin >> n; // leer la cantidad de escalones
+
+    // tabla dp donde dp[i] almacena las formas de llegar al escalon i
     vector<long long> dp(n + 1);
-    dp[0] = 1; // caso base
+
+    // casos base
+    dp[0] = 1;
     if (n >= 1) {
         dp[1] = 1;
     }
+
+    // llenar la tabla usando programacion dinamica
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    // mostrar cantidad de formas posibles 
+    cout << "formas posibles: " << dp[n] << endl;
+
+    cout << "tabla dp: ["; // se muestra la tabla dp generada
+
+    for (int i = 0; i <= n; i++) {
+        cout << dp[i];
+
+        if (i < n) {
+            cout << ", ";
+        }
+    }
+
+    cout << "]" << endl;
+
     return 0;
 }
