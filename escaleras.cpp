6296e58d629f63327185a2f3b79a4504b3878922
heydy <hcastilloa@ulasalle.edu.pe>
2026-06-22 21:54:06 -0500

    agregue caso base y implementacion de tabla dp

--- escaleras.cpp
+++ escaleras.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main() {
     int n;
-
+     //se  lee cantidad de escalones 
     cout << "Ingrese el numero de escalones: ";
     cin >> n;
-
+    // tabla dp donde almacena las formas de llegar al escalon i
+    vector<long long> dp(n + 1);
+    dp[0] = 1; // caso base
+    if (n >= 1) {
+        dp[1] = 1;
+    }
     return 0;
 }
